e4cbe389a081edc98c260ba4999041dd9b36d2e8
SanduniWathsalya <sanduniwathsalya0620@gmail.com>
2025-05-30 10:48:33 +0530

    edit

--- src/app/components/salebanner.js
+++ src/app/components/salebanner.js
@@ -2,7 +2,7 @@ export default function SaleBanner() {
   return (
     <section  className="absolute left-0 w-full z-20 transform -translate-y-24 px-8 py-2">
      
-<div className="grid grid-cols-2 gap-4 w-full px-2 sm:px-4 max-w-full mx-auto">
+<div className="grid grid-cols-2 gap-4 w-full px-2 sm:px-4 max-w-4xl mx-auto">
 
   {/* New Arrivals */}
   <div className="relative h-64 overflow-hidden shadow-lg group">
@@ -12,11 +12,11 @@ export default function SaleBanner() {
       className="w-full h-full object-fill transform group-hover:scale-105 transition duration-300"
     />
     <div className="absolute inset-0 bg-gray-500/50 p-2 sm:p-4 flex flex-col justify-center items-start w-full">
-      <h3 className="text-black text-xs sm:text-xl font-semibold mb-2 w-full ml-0 sm:ml-10">New Arrivals</h3>
-      <p className="text-black text-xs sm:text-base mb-4 w-full ml-0 sm:ml-10">
+      <h3 className="text-black text-xs sm:text-xl font-semibold mb-2 w-full ">New Arrivals</h3>
+      <p className="text-black text-xs sm:text-base mb-4 w-full ">
         Discover the latest trends!
       </p>
-      <button className="ml-0 sm:ml-10 bg-yellow-600 text-white text-xs sm:text-base px-3 py-1 sm:px-4 sm:py-2 rounded hover:bg-yellow-800 transition mt-1">
+      <button className=" bg-yellow-600 text-white text-xs sm:text-base px-3 py-1 sm:px-4 sm:py-2 rounded hover:bg-yellow-800 transition mt-1">
         Shop Now
       </button>
     </div>
@@ -30,9 +30,9 @@ export default function SaleBanner() {
       className="w-full h-full object-fill transform group-hover:scale-105 transition duration-300"
     />
     <div className="absolute inset-0 bg-gray-500/50 p-2 sm:p-4 flex flex-col justify-center items-start w-full">
-      <h3 className="text-black text-xs sm:text-xl font-semibold mb-2 w-full ml-0 sm:ml-10">Hot Sale</h3>
-      <p className="text-black text-xs sm:text-base mb-4 w-full ml-0 sm:ml-10">Limited time offers!</p>
-      <button className="ml-0 sm:ml-10 bg-yellow-600 text-white text-xs sm:text-base px-3 py-1 sm:px-4 sm:py-2 rounded hover:bg-yellow-800 transform hover:scale-105 transition mt-1">
+      <h3 className="text-black text-xs sm:text-xl font-semibold mb-2 w-full ">Hot Sale</h3>
+      <p className="text-black text-xs sm:text-base mb-4 w-full ">Limited time offers!</p>
+      <button className=" bg-yellow-600 text-white text-xs sm:text-base px-3 py-1 sm:px-4 sm:py-2 rounded hover:bg-yellow-800 transform hover:scale-105 transition mt-1">
         Shop Now
       </button>
     </div>
